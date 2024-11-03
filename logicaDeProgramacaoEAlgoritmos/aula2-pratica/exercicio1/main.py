c188b31c7c4f94d59b96c979e3272d0547bfdc1f
Amanda Kado <amandakado.ak@gmail.com>
2024-11-02 22:26:44 -0300

    exercicio1

--- logicaDeProgramacaoEAlgoritmos/aula2-pratica/exercicio1/main.py
+++ logicaDeProgramacaoEAlgoritmos/aula2-pratica/exercicio1/main.py
@@ -1,1 +1,7 @@
 #Desenvolva um algoritmo que solicite ao usuário o preço de um produto e um percentual de desconto a ser aplicado a ele. Calcule e exiba o valor do desconto e o preço final do produto
+precoProduto = float(input('Qual o preço do produto?'))
+percentualDesconto = float(input('Qual o percentual do desconto?'))
+valorDesconto =  precoProduto  * (percentualDesconto / 100)
+valorFinalProduto = precoProduto - valorDesconto
+
+print(f'Você vai ter um desconto de R${valorDesconto}, ficando só R${valorFinalProduto}')
