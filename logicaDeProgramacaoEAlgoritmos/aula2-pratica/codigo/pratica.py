d5baadd00df24c310f15c2557a9c9fd56c3290ed
Amanda Kado <amandakado.ak@gmail.com>
2024-11-02 22:08:23 -0300

    finalizando primeira aula pratica

--- logicaDeProgramacaoEAlgoritmos/aula2-pratica/codigo/pratica.py
+++ logicaDeProgramacaoEAlgoritmos/aula2-pratica/codigo/pratica.py
@@ -25,3 +25,22 @@ b = 4
 c = a * a + b * b
 
 print(c)
+
+#STRINGS
+
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+
+# 'ant bat cod'
+print(f'{s1} {s2} {s3}')
+# 'ant ant ant ant ant ant ant ant ant ant'
+print( (s1 + ' ') * 10 )
+# 'ant bat bat cod cod cod'
+print(f'{s1} ' + f'{s2} ' * 2 + f'{s3} ' * 3 )
+# 'ant bat ant bat ant bat ant bat ant bat ant bat ant bat'
+print(f'{s1} {s2} ' * 7)
+# 'batbatcod batbatcod batbatcod batbatcod batbatcod'
+print(f'{s2*2}{s3} ' * 5)
+
+
---
+++ logicaDeProgramacaoEAlgoritmos/aula2-pratica/exercicio1/main.py
@@ -0,0 +1,1 @@
+#Desenvolva um algoritmo que solicite ao usuário o preço de um produto e um percentual de desconto a ser aplicado a ele. Calcule e exiba o valor do desconto e o preço final do produto
