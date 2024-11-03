30d888a60b4b7ef928c3229528e82a8962889232
Amanda Kado <amandakado.ak@gmail.com>
2024-11-02 22:39:18 -0300

    exercicio2
    Feito sozinho o exercicio 2, verificando correcao posteriormente

--- logicaDeProgramacaoEAlgoritmos/aula2-pratica/exercicio2/main.py
+++ logicaDeProgramacaoEAlgoritmos/aula2-pratica/exercicio2/main.py
@@ -1,2 +1,10 @@
 #Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0.15 por km rodado
 
+kmPercorrido = int(input('Quantidade de km percorridos pelo carro alugado: '))
+diasAlugado = int(input('O carro foi alugado por quantos dias? '))
+
+custoDiario = diasAlugado * 60
+custoKmRodado = kmPercorrido * 0.15
+precoTotal = custoDiario + custoKmRodado
+
+print(precoTotal)
