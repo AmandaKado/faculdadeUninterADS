ad3aaf1baa115de611faa6e24162069f582cea0e
Amanda Kado <amandakado.ak@gmail.com>
2024-11-02 21:34:38 -0300

    fazendo exercicio 1 de python

---
+++ logicaDeProgramacaoEAlgoritmos/aula2-pratica/codigo/main.py
@@ -0,0 +1,27 @@
+#EMPRESSÕES ALGÉBRICAS
+
+#somar 5 numeros
+print( 1 + 2 + 3 + 4 + 5 )
+#somar a media dos numeros 23, 19 e 31
+print( (23 + 19 + 31) / 3 )
+#o numero de vezes que o numero 73 cabe em 403
+print( 403 // 73 )
+#A sobra quando 403 é dividido por 73
+print( 403 % 73 )
+#2 elevado à 10 potencia
+print( 2 ** 10 )
+#o valor absoluto da diferença entre 54 e 57
+print( abs(54 - 57) )
+#menor valor entre 34,29 e 31
+print( min(34, 29, 31) )
+
+#ATRIBUIÇÃO
+
+#atribuir o valor inteiro 3 á variavel a
+a = 3
+#atribuir o valor 4 à variável b
+b = 4
+#atribuir à variável c o valor da empressão a*b + b*b 
+c = a * a + b * b
+
+print(c)
