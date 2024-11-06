e4d01ae316c0e46c9ec196830f6829702b885869
Amanda Kado <amandakado.ak@gmail.com>
2024-11-06 20:36:25 -0300

    pratica2

---
+++ logicaDeProgramacaoEAlgoritmos/aula2-pratica/aulaPratica/pratica2.py
@@ -0,0 +1,20 @@
+# O somatório de 2 com 2 é menor que 4
+if ( 2 * 2 < 4 ):
+    print('verdadeiro a') 
+else:
+    print('falso A')
+# O valor 7 // 3 é igual a 1 + 1 
+if ( 7 // 3 == 1 + 1 ):
+    print('verdadeiro b')
+else:
+    print('falso b')
+# A soma de 3 elevador ao quadrado com 4 elevado ao quadrado é igual a 25
+if ( 3 ** 2 + 4 ** 2 == 25 ):
+    print('verdadeiro c')
+else:
+    print('falso c')
+# A soma de 2, 4 e 6 é maior que 12
+if ( (2 + 4 + 6) > 12 ):
+    print('verdadeiro d')
+else:
+    print('falso d')
