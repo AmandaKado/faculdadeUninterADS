77b47f779b0cbe8b1de09bcb398931590d4738d0
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2024-11-22 13:28:43 -0300

    Update questao1.py

--- logicaDeProgramacaoEAlgoritmos/trabalho/questao1.py
+++ logicaDeProgramacaoEAlgoritmos/trabalho/questao1.py
@@ -39,6 +39,9 @@ Elabore um programa em Python que:
     H. Deve-se apresentar na saída de console a utilização do sistema informando uma idade maior ou igual a 29 anos, apresentando na saída de console o valorMensal do plano [EXIGÊNCIA DE SAÍDA DE CONSOLE 2 de 2];
     
 """
+
+# CÓDIGO PRINCIPAL
+
 # Print simples para mostrar quem desenvolveu o sistema:
 print("Sistema desenvolvido por Amanda Mayumi Kado") 
 
@@ -48,26 +51,38 @@ valorBase = float(input("Insira o valor base do plano: "))
 idade = int(input("Idade do cliente: ")) 
 # Variável 'porcentagem' para armazenar a porcentagem:
 porcentagem = 0
-# Variável 'valorMensal' referente ao valor que será cobrado mensalmente do cliente:
-valorMensal = valorBase * porcentagem
 
 
 # Se a idade for maior ou igual que 0 e menor que 19, o valor será de 100% do valor base do plano (100 / 100):
 if(idade >= 0 and idade < 19):
+    porcentagem = 100
+    valorMensal = valorBase * (porcentagem / 100)
+
 # Se a idade for maior ou igual que 19 e menor que 29, o valor será de 150% do valor base do plano (150 / 100):
 elif(idade >= 19 and idade < 29):
+    porcentagem = 100
+    valorMensal = valorBase * (porcentagem / 100)
     
 # Se a idade for maior ou igual que 29 e menor que 39, o valor será de 225% do valor base do plano (225 / 100):
 elif(idade >= 29 and idade < 39):
+    porcentagem = 100
+    valorMensal = valorBase * (porcentagem / 100)
     
 # Se a idade for maior ou igual que 39 e menor que 49, o valor será de 240% do valor base do plano (240 / 100):
 elif(idade >= 39 and idade < 49):
+    porcentagem = 100
+    valorMensal = valorBase * (porcentagem / 100)
     
 # Se a idade for maior ou igual que 49 e menor que 59, o valor será de 350% do valor base do plano (350 / 100):
 elif(idade >= 49 and idade < 59):
+    porcentagem = 100
+    valorMensal = valorBase * (porcentagem / 100)
     
 # Se a idade for maior ou igual que 59, o valor será de 600% do valor base do plano (600 / 100):
 elif(idade >= 59):
+    porcentagem = 100
+    valorMensal = valorBase * (porcentagem / 100)
     
 # Se valor de idade não é válido executa:
 else:
+    
