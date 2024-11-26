61b3da4c796cf7adae8649d3d0a639f7b68a19ca
Amanda Kado <amandakado.ak@gmail.com>
2024-11-25 22:30:17 -0300

    questao 2 iniciei

--- logicaDeProgramacaoEAlgoritmos/trabalho/questao2.py
+++ logicaDeProgramacaoEAlgoritmos/trabalho/questao2.py
@@ -41,5 +41,61 @@ Elabore um programa em Python que:
     L. Deve-se apresentar na saída de console um pedido com duas opções sabores diferentes e com tamanhos diferentes [EXIGÊNCIA DE SAÍDA DE CONSOLE 4 de 4]; 
 
 """
+# Variável criada para montar menu da pizzaria:
+enfeite = "*" * 65 
+print(enfeite)
+#  Mostra quem desenvolveu o Sistema:
+print("-" * 10 + " Bem-vindos a Pizzaria da Amanda Mayumi Kado " + "-" * 10) 
+print(enfeite)
+# Mostra no console o menu para o cliente:
+print("-" * 28 + " CARDÁPIO " + "-" * 27) 
+print(enfeite)
+print("----|   Tamanho  |  Pizza Salgada (PS)  |  Pizza doce (PD)  |----")
+print("----|      P     |  R$ 30.00            |  R$ 34.00         |----")
+print("----|      M     |  R$ 45.00            |  R$ 48.00         |----")
+print("----|      G     |  R$ 60.00            |  R$ 66.00         |----")
+print(enfeite)
 
-print("Bem-vindos a Pizzaria da Amanda Mayumi Kado") #Print simples para mostrar quem desenvolveu o sistema
+# Variável acumuladora para somar o total do pedido:
+valorTotal = 0
+
+while True:
+    # Variável para armazenar o sabor escolhido
+    while True:  # Loop para garantir uma entrada válida
+        sabor = input("Escolha o sabor da pizza (PS para Salgada ou PD para Doce): ").upper() # Função upper para garantir que mesmo que o usuário insira caractere minusculo seja considerado
+        if sabor in ["PS", "PD"]:  # Verifica se o sabor está entre os válidos
+            break  # Sai do loop se o sabor for válido
+        else:
+            print("Sabor inválido. Tente novamente.")  # Informa o erro e continua no loop
+
+    # Variável para armazenar o tamanho escolhido
+    while True:  # Loop para garantir uma entrada válida
+        tamanho = input("Escolha o tamanho da pizza (P, M ou G): ").upper()
+        if tamanho in ["P", "M", "G"]:  # Verifica se o tamanho está entre os válidos
+            break  # Sai do loop se o tamanho for válido
+        else:
+            print("Tamanho inválido. Tente novamente.")  # Informa o erro e continua no loop
+
+    if sabor == "PS":
+        if tamanho == "P":
+            preco = 30
+        elif tamanho == "M":
+            preco = 45
+        else:
+            preco = 60
+    else:
+        if tamanho == "P":
+            preco = 34
+        elif tamanho == "M":
+            preco = 48
+        else:
+            preco = 66
+            
+    valorTotal += preco
+
+    continuar = input("Deseja pedir mais alguma coisa? [S/N]")
+
+    if continuar == "S":
+        continue    
+    else:
+        break
