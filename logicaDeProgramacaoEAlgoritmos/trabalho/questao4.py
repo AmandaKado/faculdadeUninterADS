666a6dac0380592500a60185bd7e1ed239138f10
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2024-12-05 12:41:31 -0300

    Update questao4.py

--- logicaDeProgramacaoEAlgoritmos/trabalho/questao4.py
+++ logicaDeProgramacaoEAlgoritmos/trabalho/questao4.py
@@ -3,28 +3,36 @@ def exibir_desenvolvedor():
 
 # Função para exibir menu
 def exibir_menu(): 
-    print("1) Cadastrar Contato")
-    print("2) Consultar Contato")
-    print("3) Remover Contato")
-    print("4) Encerrar Programa\n")
-    
-    resposta = int(input("Envie somente o número [1, 2, 3, 4] >> "))
-    
-    try:
-        match resposta:
-            case 1:
-                print("1")
-            case 2: 
-                print("2")
-            case 3: 
-                print("3")
-            case 4:
-                print("4")
-            case _:
-                print("\nValor inválido! Insira um número válido\n")
-    except ValueError:
-        print("Insira um número de 1 à 4!\n")
-    
+    while True:
+        print("1) Cadastrar Contato")
+        print("2) Consultar Contato")
+        print("3) Remover Contato")
+        print("4) Encerrar Programa\n")
+        
+        resposta = int(input("Envie somente o número [1, 2, 3, 4] >> "))
+        
+        try:
+            match resposta:
+                case 1:
+                    print("1")
+                    break
+                case 2: 
+                    print("2")
+                    break
+                case 3: 
+                    print("3")
+                    break
+                case 4:
+                    print("4")
+                    break
+                case _:
+                    print("\nValor inválido! Insira um número válido\n")
+                    continue
+        except ValueError:
+            print("Insira um número de 1 à 4!\n")
+            continue
+        break
+
 # Código principal para execuução do programa
 exibir_desenvolvedor()
 
